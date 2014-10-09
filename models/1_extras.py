e8c4588666e3cedfd5a4250edf02f3b707f6dcbe
fernando <fndiaz02@gmail.com>
2014-10-09 09:10:03 -0300

    1.0.0.19

--- models/1_extras.py
+++ models/1_extras.py
@@ -105,3 +105,4 @@ def peers_fop2():
 		arq.write('extension=%s\n' %(ramal.ramal_virtual))
 		arq.write('label=%s\n' %(ramal.nome))
 		arq.write('context=ramais\n\n')
+		commands.getoutput("/etc/init.d/fop2 reload")

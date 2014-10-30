889d812cdce2b090d79bf4ac4228a9b52e87211c
fernando <fndiaz02@gmail.com>
2014-10-30 09:30:02 -0200

    1.0.0.22

--- controllers/bilhetes.py
+++ controllers/bilhetes.py
@@ -5,12 +5,25 @@ def busca_chamadas():
 	response.title = 'Bilhetes'
 	response.marca=['Bilhetes']
 
+	depta=db(db.f_departamentos.mostrar == True).select(db.f_departamentos.departamento)
+
+	#Criando lista para select departamentos
+	list_dept=[]
+	for dept in depta:
+		dept_nome = 'dept_%s' %(dept.departamento)
+		print dept_nome
+		if (auth.has_membership(dept_nome) or auth.has_membership('administrador')):
+			list_dept.append(dept.departamento)
+	print list_dept
+
 	form = SQLFORM.factory(
 		Field("origem"),
 		Field("destino"),
-		Field("data_inicio"),
-		Field("data_fim"),
-		Field("tipo", requires=IS_IN_SET(['Saintes', 'Entrantes', 'Ramais']))
+		Field("data_inicio", requires=IS_NOT_EMPTY()),
+		Field("data_fim", requires=IS_NOT_EMPTY()),
+		Field("usuario"),
+		Field("departamento", requires=IS_EMPTY_OR(IS_IN_SET(list_dept))),
+		Field("tipo", requires=IS_IN_SET(['Todas', 'Saintes', 'Entrantes', 'Ramais']))
 	)
 	for input in form.elements():
 		input['_class'] = 'form-control'
@@ -39,6 +52,8 @@ def query_monta(dado):
 	data_fim = dado.data_fim +"23:59:00"
 	print data_inicio
 	print data_fim
+	print 'DEPT'
+	print dado.departamento
 
 		
 	query = (db.f_bilhetes_chamadas.origem.like ('%'+dado.origem+'%'))&\
@@ -46,12 +61,19 @@ def query_monta(dado):
 	((db.f_bilhetes_chamadas.horario >= data_inicio) 
 		& (db.f_bilhetes_chamadas.horario <= data_fim))
 
-	if dado.tipo == 'Saintes':
-		query=query & (db.f_bilhetes_chamadas.id_destino > 0)
-	if dado.tipo == 'Entrantes':
-		query=query & (db.f_bilhetes_chamadas.id_destino == -1)
-	if dado.tipo == 'Ramais':
-		query=query & (db.f_bilhetes_chamadas.id_destino == 0)
+	if dado.tipo != 'Todas':
+		if dado.tipo == 'Saintes':
+			query=query & (db.f_bilhetes_chamadas.id_destino > 0)
+		if dado.tipo == 'Entrantes':
+			query=query & (db.f_bilhetes_chamadas.id_destino == -1)
+		if dado.tipo == 'Ramais':
+			query=query & (db.f_bilhetes_chamadas.id_destino == 0)
+
+	if dado.departamento != '':
+		query=query & (db.f_bilhetes_chamadas.departamento == dado.departamento)
+
+	if dado.usuario != '':
+		query=query & (db.f_bilhetes_chamadas.nome_origem.like ('%'+dado.usuario+'%'))
 
 	##Adiciona query por departamento
  	depta=db(db.f_departamentos.mostrar == True).select(db.f_departamentos.departamento)
@@ -75,6 +97,7 @@ def query_monta(dado):
 	
 	query = query & query_dept
 	print query
+	#query=(db.f_bilhetes_chamadas.horario >= '01-10-2013')
 	return query
 
 
